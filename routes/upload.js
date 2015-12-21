8520c8c8daed483158bdb11078db8bbf72ad8aa9
cecidisi <cecidisi@gmail.com>
2015-12-21 16:57:56 +0100

    working on file receive

--- routes/upload.js
+++ routes/upload.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({ dest: '../uploads/' });
 
 /* GET upload. */
 
 
 router.get('/', function(req, res, next){
     res.send('upload something');
+})
+
+
+router.post('/', upload.array(), function(req, res, next){
+    if (req.files) {
+        res.send(req.files);
+    }
+    else {
+        res.send('No files');
+
+    }
 })
 
 module.exports = router;
