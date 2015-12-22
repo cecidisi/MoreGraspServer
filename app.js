27265714a07c4ccc8e704bb4001635411d3d0e10
cdisciascio <cecidisi@gmail.com>
2015-12-22 16:38:40 +0100

    file upload working (only single)

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fsSync = require('fs-sync');
+
+//  Create uploads folder
+fsSync.mkdir('uploads', function(err){
+    if(err) console.log('Cannot create folder --> ', err);
+    else console.log('Folder created');
+});
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -24,9 +32,57 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use('/', routes);
 app.use('/users', users);
-app.use('/upload', uploads);
+//app.use('/upload', uploads);
+
+
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/')
+    },
+    filename: function (req, file, cb) {
+        console.log(file);
+//        cb(null, file.fieldname + '-' + Date.now())
+        cb(null, file.originalname);
+    }
+})
+
+//var upload = multer({ dest: './uploads/', limits: { fieldSize: 52428800, fileSize: 52428800 } });
+var upload = multer({ storage: storage, limits: { fieldSize: 52428800, fileSize: 52428800, files: 3 } });
+
+app.post('/upload', upload.single('video'), function(req, res, next){
+    console.log('entra single video');
+    if (req.file) {
+        console.log(req.file);
+        res.status(200).send('File uploaded');
+    }
+    else {
+        res.status(200).send('No files');
+
+    }
+});
+
+app.post('/upload', upload.array('videos', 3), function(req, res, next){
+    console.log('entra multiple videos');
+    if (req.files) {
+        console.log(req.files);
+        res.status(200).send('Multiple Files uploaded');
+    }
+    else {
+        res.status(200).send('No files');
+
+    }
+});
+
+
 
 
 // catch 404 and forward to error handler
@@ -43,6 +99,9 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
+    console.log(req.file);
+    console.log(req.files);
+    console.log(err);
     res.render('error', {
       message: err.message,
       error: err
--- routes/upload.js
+++ routes/upload.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
-var upload = multer({ dest: '../uploads/' });
+var upload = multer({ dest: '../uploads/', limits: { fieldSize: 999999, fileSize: 999999 } });
 
-/* GET upload. */
+
 
 
 router.get('/', function(req, res, next){
@@ -11,14 +11,37 @@ router.get('/', function(req, res, next){
 })
 
 
-router.post('/', upload.array(), function(req, res, next){
+/* POST upload. */
+
+router.post('/', upload.array('videos', 30), function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    console.log('entra');
     if (req.files) {
+        console.log(req.files);
         res.send(req.files);
     }
     else {
         res.send('No files');
 
     }
-})
+});
+
+
+
+router.post('/', upload.single('video'), function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    console.log('entra');
+    if (req.file) {
+        console.log(req.file);
+        res.send(req.files);
+    }
+    else {
+        res.send('No files');
+
+    }
+});
+
 
 module.exports = router;
